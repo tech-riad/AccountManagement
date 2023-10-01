20d7431833e302cc5a2a338eb99b473381ae5083
Md. Riad Ahmed <110854493+tech-riad@users.noreply.github.com>
2023-10-01 17:39:39 +0600

    accounts-model-mc-created

---
+++ database/migrations/2023_10_01_113051_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('account_method');
+            $table->string('customer_name');
+            $table->date('payment_date');
+            $table->decimal('amount', 10, 2);
+            $table->string('category_type');
+            $table->string('created_by');
+            $table->longText('description');
+            $table->string('product_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
